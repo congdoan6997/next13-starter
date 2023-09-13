a3355d2ab5fe91eb2bdc27825207d6b7f102017d
PhDBui <congdoan6997@gmail.com>
2023-09-13 18:17:41 +0700

    create header, table, footer components

---
+++ src/components/app.footer.tsx
@@ -0,0 +1,9 @@
+const AppFooter = () => {
+  return (
+    <div style={{ background: "#ddd", textAlign: "center", padding: "5px 0" }}>
+      App create by PhDBui
+    </div>
+  );
+};
+
+export default AppFooter;
